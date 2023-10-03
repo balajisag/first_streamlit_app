fc6adf8ba3f34e39dfa01440d92cb0b4948d0df7
balajisag <73623098+balajisag@users.noreply.github.com>
2023-10-03 12:45:07 +1000

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
